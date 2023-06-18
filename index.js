44e43868f0ec781f75be434705886dc1722d03ef
Njoki-Mwangi <faithgracem96@gmail.com>
2023-06-18 23:17:45 +0300

    first trial

---
+++ index.js
@@ -0,0 +1,17 @@
+let marks = 99;
+// if(marks >= 0 && marks <= 100){
+//     return 'Valid Marks'
+// }
+// if(marks > 79){
+//     return 'Your grade is A'
+// } else if(marks >= 60 && marks <= 79){
+//     return 'Your grade is B'
+// } else if (marks >= 59 && marks <= 49){
+//     return 'Your grade is C'
+// }else if(marks >= 40 && marks < 49){
+//     return 'Your grade is D'
+// }else{
+//     return 'Your grade is E'
+// }
+
+console.log(marks)
---
+++ speedDetector.js
@@ -0,0 +1,18 @@
+function speedDetector(speed,points){
+if(speed <= 70){
+    return "Ok"
+} else (speed > 70);{
+    return points ((speed - 70) / 5)
+}
+if(points > 12){
+    return 'License suspended!'
+}
+}
+
+//  //points = ((speed - 70) / 5)
+// //return `Demerit &{points}`
+// return points '((speed - 70) / 5)`
+// }
+// if(points > 12){
+//     return 'License suspended' 
+// }
