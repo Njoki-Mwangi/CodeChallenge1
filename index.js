ca7bc50d2d9dce410a37a7439bf6094ded8793c3
Njoki-Mwangi <faithgracem96@gmail.com>
2023-06-18 23:51:55 +0300

    my first commit

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-let marks = 99;
-// if(marks >= 0 && marks <= 100){
-//     return 'Valid Marks'
-// }
-// if(marks > 79){
-//     return 'Your grade is A'
-// } else if(marks >= 60 && marks <= 79){
-//     return 'Your grade is B'
-// } else if (marks >= 59 && marks <= 49){
-//     return 'Your grade is C'
-// }else if(marks >= 40 && marks < 49){
-//     return 'Your grade is D'
-// }else{
-//     return 'Your grade is E'
-// }
+let marks = prompt("Enter student marks")
+if(marks >= 0 && marks <= 100){
+     return 'Valid Marks'
+}
+let grade;
+if (marks >= 80) {
+  grade = "A";
+} else if (marks >= 60 && marks <= 79) {
+  grade = "B";
+} else if (marks >= 50 && marks <= 59) {
+  grade = "C";
+} else if (marks >= 40 && marks <= 49) {
+  grade = "D";
+} else {
+  grade = "E";
+}
 
-console.log(marks)
+console.log("Grade");
+
+
+
--- speedDetector.js
+++ speedDetector.js
@@ -1,13 +1,19 @@
-function speedDetector(speed,points){
+function speedDetector(speed){
 if(speed <= 70){
     return "Ok"
-} else (speed > 70);{
-    return points ((speed - 70) / 5)
-}
+} else {
+let points = (speed - 70) / 5 
 if(points > 12){
     return 'License suspended!'
+}else{
+    return "Points: " + points.toFixed(2)
 }
 }
+}
+console.log(speedDetector(80))
+console.log(speedDetector(70))
+console.log(speedDetector(90))
+console.log(speedDetector(120))
 
 //  //points = ((speed - 70) / 5)
 // //return `Demerit &{points}`
